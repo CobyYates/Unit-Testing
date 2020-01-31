385a695cf9849ee573a7a36479c853c0fd057a7b
cobylyates <cobylyates@gmail.com>
2020-01-31 06:18:03 -0700

    Added fizzbuzz function, tests not passing

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,17 +1,17 @@
-const {} = require("./fizzbuzz.js");
+const {fizzBuzz} = require("./fizzbuzz.js");
 
 test("number divisible by 3", () => {
-  expect(fizzbuzz(6)).toBe("fizz");
+  expect(fizzBuzz(6)).toBe("fizz");
 });
 
 test("number divisible by 5", () => {
-  expect(fizzbuzz(10)).toBe("buzz");
+  expect(fizzBuzz(10)).toBe("buzz");
 });
 
 test("number divisible by 3 and 5", () => {
-  expect(fizzbuzz(30)).toBe("fizzbuzz");
+  expect(fizzBuzz(30)).toBe("fizzbuzz");
 });
 
 test("number divisible by 3 and 5 at once", () => {
-  expect(fizzbuzz(4)).toBe(4);
+  expect(fizzBuzz(4)).toBe(4);
 });
